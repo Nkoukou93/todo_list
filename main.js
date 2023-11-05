ef4773dcaded0419ac4366c0d1adfe3bd21a2e5b
Nkoukou93 <guilaineyeman93@gmail.com>
2023-11-05 21:30:42 +0100

    todolist

---
+++ main.js
@@ -0,0 +1,54 @@
+// Créez un bouton "Fermer" et ajoutez-le à chaque élément de la liste
+var maliste = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < maliste.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  maliste[i].appendChild(span);
+}
+
+// Clic sur un bouton de fermeture pour masquer l'élément de liste actuel
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+//Ajouter un symbole "coché" lorsqu'on clique sur un élément de la liste
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+//Créer un nouvel élément de liste en cliquant sur le bouton "Ajouter"
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("ecrivez quelque chose!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
